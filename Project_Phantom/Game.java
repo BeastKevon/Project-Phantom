4ae8e153247774f90a5e841b46792a30f45346d4
BeastKevon <beastkevon@gmail.com>
2018-04-18 08:14:30 -0700

    Add files via upload

---
+++ Project_Phantom/Game.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+/**
+ * Write a description of class Game here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Game
+{
+    public Game()
+    {
+        
+    }
+    
+    
+}
